e830a28ed6f1a72590dfbcdd52ae431cee912f78
Cassio Faleiro <cassio.faleiro@icloud.com>
2020-06-18 19:57:45 -0300

    Remove Function

--- layout/specs/scripts.js
+++ layout/specs/scripts.js
@@ -1,8 +1,14 @@
 const modalOverlay = document.querySelector('.modal__overlay')
 const receps = document.querySelectorAll('.recipe__content')
 
+
 for(let recep of receps){
     recep.addEventListener("click", function(){
         modalOverlay.classList.add('active')
     })
 }
+
+// fecha o modal removendo a class active
+document.querySelector(".close__modal").addEventListener("click", function(){
+    modalOverlay.classList.remove("active")
+})
