78c74753741749d4fdb38e70f34d919860487083
Cassio Faleiro <cassio.faleiro@icloud.com>
2020-06-22 20:42:29 -0300

    Complete

--- layout/specs/scripts.js
+++ layout/specs/scripts.js
@@ -5,8 +5,12 @@ const receps = document.querySelectorAll('.recipe__content')
 for(let recep of receps){
     recep.addEventListener("click", function(){
         const img = recep.getAttribute("id")
+        const recipesName = recep.querySelector(".recipes__name").textContent
+        const chefName = recep.querySelector(".chef").textContent
         modalOverlay.classList.add('active')
         modalOverlay.querySelector("img").src = `/layout/assets/${img}.png`
+        modalOverlay.querySelector("h4").textContent = recipesName
+        modalOverlay.querySelector("p").textContent = chefName
     })
 }
 
