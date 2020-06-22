305743221727be5f921d038ee2f1d6629a439623
Cassio Faleiro <cassio.faleiro@icloud.com>
2020-06-21 23:12:06 -0300

    Select Receps

--- layout/specs/scripts.js
+++ layout/specs/scripts.js
@@ -4,11 +4,13 @@ const receps = document.querySelectorAll('.recipe__content')
 
 for(let recep of receps){
     recep.addEventListener("click", function(){
+        const img = recep.getAttribute("id")
         modalOverlay.classList.add('active')
+        modalOverlay.querySelector("img").src = `/layout/assets/${img}.png`
     })
 }
 
 // fecha o modal removendo a class active
-document.querySelector(".close__modal").addEventListener("click", function(){
+modalOverlay.addEventListener("click", function(){
     modalOverlay.classList.remove("active")
 })
